59b00e838762501f67c953505510e2243d6ab4fd
kazunoko1606 <kazu.negi0719@gmail.com>
2022-01-17 21:01:28 +0900

    Integrate incorrect answers

--- docs/script.js
+++ docs/script.js
@@ -22,22 +22,10 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.highscore').textContent = highscore;
     }
 
-  // 入力された数字が正解より大きい時
-  } else if (inputNumber > secretNumber) {
+  // 不正解の時
+  } else if (inputNumber !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'もっと小さいよ📉😏';
-      score--;
-      document.querySelector('.score').textContent = score; 
-    } else {
-      document.querySelector('.message').textContent = 'ゲームオーバー🤪';
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = '#DC3544';
-    }
-
-  // 入力された数字が正解より小さい時
-  } else if (inputNumber < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'もっと大きいよ📈😏';
+      document.querySelector('.message').textContent = inputNumber > secretNumber ? 'もっと小さいよ📉😏' : 'もっと大きいよ📈😏';
       score--;
       document.querySelector('.score').textContent = score; 
     } else {
