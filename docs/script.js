a463f2f115dcfd08bdbfd75402204c9c2fa513f2
kazunoko1606 <kazu.negi0719@gmail.com>
2022-01-17 11:06:20 +0900

    Add click event

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const inputNumber = Number(document.querySelector('.inputNumber').value);
+ 
+});
